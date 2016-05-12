34188015078ff5e1afc416ce45c812aa756ef2c3
Discosultan <j2v@neti.ee>
2016-05-12 22:12:30 +0300

    Update menu

--- menu.js
+++ menu.js
@@ -1,21 +1,43 @@
 function Menu(container, simulation) {
-    var CAMERA_ROTATION_AMOUNT = Math.PI * 0.5;
+    var CAMERA_ROTATION_AMOUNT = Math.PI * 0.25;
     var CAMERA_ROTATION_SECONDS = 0.4;
+    var MINIMUM_NUMBER_OF_ELEMENTS = 5;
 
     var elements = container.getElementsByClassName('menu-element');
+    var elementCount = elements.length;
+    if (elementCount < MINIMUM_NUMBER_OF_ELEMENTS) {
+        for (var i = 0; i < elementCount; i++) {
+            var element = elements[i];
+            var newElement = element.cloneNode(true);
+            container.appendChild(newElement);
+        }        
+    }
+
     for (var i = 0; i < elements.length; i++) {
         var element = elements[i];
         element.dataset.index = i;
         element.addEventListener('click', function() {
             var index = parseInt(this.dataset.index);
-            if (index === 1) { // left
+            if (index === 0) { // left
                 rotateElementsRight(elements);
-            } else if (index === 3) { // right
+            } else if (index === 2) { // right
                 rotateElementsLeft(elements);
             }
         });
     }
+    assignElementsClasses(elements);
 
+    window.addEventListener('keydown', function(evt) {
+        // if (self.transitionInProgress) return;
+
+        if (evt.keyCode === 37) { // Left arrow.
+            rotateElementsLeft(elements);
+        } else if (evt.keyCode === 39) { // Right arrow.
+            rotateElementsRight(elements);
+        }
+    }, false);
+
+    var transitionInProgress = false;
     var previousTimestamp = 0;
     var processes = [];
     requestAnimationFrame(update);
@@ -31,6 +53,8 @@ function Menu(container, simulation) {
                 processes.splice(i, 1);
             }
         }
+
+        self.transitionInProgress = processes.length > 0;
 
         requestAnimationFrame(update);
     }
@@ -63,6 +87,7 @@ function Menu(container, simulation) {
     }
 
     function assignElementsClasses(elements) {
+        var lastIndex = elements.length - 1;
         for (var i = 0; i < elements.length; i++) {
             var element = elements[i];
             element.classList.remove('left-out');
@@ -74,22 +99,19 @@ function Menu(container, simulation) {
             var index = parseInt(element.dataset.index);
             switch (index) {
                 case 0:
-                    element.classList.add('left-out');
-                    break;
-                case 1:
                     element.classList.add('left');
                     break;
-                case 2:
+                case 1:
                     element.classList.add('center');
                     break;
-                case 3:
+                case 2:
                     element.classList.add('right');
                     break;
-                case 4:
+                case 3:
                     element.classList.add('right-out');
                     break;
                 default:
-                    element.classList.add('hide');
+                    element.classList.add(index === lastIndex ? 'left-out' : 'hide');
                     break;
             }
         }
@@ -99,6 +121,7 @@ function Menu(container, simulation) {
         var self = this;
         this.finished = false;
         this.elapsedSeconds = 0;
+        this.rotAmountProgess = 0;
 
         var bezier = new UnitBezier(0.250, 0.100, 0.250, 1.000);
 
@@ -108,39 +131,36 @@ function Menu(container, simulation) {
                 self.finished = true;
             }
 
-            var progress = self.elapsedSeconds / seconds;
-            var y = bezier.sampleCurveY(progress);
-            simulation.cameraRotation += y * deltaSeconds;
+            var age = self.elapsedSeconds / seconds;
+            var progress = bezier.sampleCurveY(age);
+            var newRotAmountProgess = lerp(0, amount, progress);
 
-            // simulation.cameraRotation += deltaSeconds * amount / seconds;
+            simulation.cameraRotation += newRotAmountProgess - self.rotAmountProgess;
+            self.rotAmountProgess = newRotAmountProgess;
         }
 
-        // function lerp(v0, v1, t) {
-        //     return v0 + t*(v1 - v0);
-        // }
-    }
+        function lerp(v0, v1, t) {
+            return v0 + t*(v1 - v0);
+        }
 
-    /**
-    * Solver for cubic bezier curve with implicit control points at (0,0) and (1.0, 1.0)
-    * Ref: http://stackoverflow.com/a/11697909/1466456
-    */
-    function UnitBezier(p1x, p1y, p2x, p2y) {
-        // pre-calculate the polynomial coefficients
-        // First and last control points are implied to be (0,0) and (1.0, 1.0)
-        this.cx = 3.0 * p1x;
-        this.bx = 3.0 * (p2x - p1x) - this.cx;
-        this.ax = 1.0 - this.cx -this.bx;
+        /**
+        * Solver for cubic bezier curve with implicit control points at (0,0) and (1.0, 1.0)
+        * Ref: http://stackoverflow.com/a/11697909/1466456
+        */
+        function UnitBezier(p1x, p1y, p2x, p2y) {
+            // pre-calculate the polynomial coefficients
+            // First and last control points are implied to be (0,0) and (1.0, 1.0)
+            this.cx = 3.0 * p1x;
+            this.bx = 3.0 * (p2x - p1x) - this.cx;
+            this.ax = 1.0 - this.cx -this.bx;
 
-        this.cy = 3.0 * p1y;
-        this.by = 3.0 * (p2y - p1y) - this.cy;
-        this.ay = 1.0 - this.cy - this.by;
-    }
+            this.cy = 3.0 * p1y;
+            this.by = 3.0 * (p2y - p1y) - this.cy;
+            this.ay = 1.0 - this.cy - this.by;
+        }
 
-    UnitBezier.prototype.sampleCurveY = function (t) {
-        return ((this.ay * t + this.by) * t + this.cy) * t;
-    }
-
-    UnitBezier.prototype.sampleCurveX = function(t) {
-        return ((this.ax * t + this.bx) * t + this.cx) * t;
+        UnitBezier.prototype.sampleCurveY = function (t) {
+            return ((this.ay * t + this.by) * t + this.cy) * t;
+        }
     }
 }
