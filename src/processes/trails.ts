88eff82b5cefca2a152ec77193fdb35c0b06a72d
discosultan <jaanusvarus@gmail.com>
2017-11-16 00:00:38 +0100

    Fix trail for IE/Edge

--- src/processes/trails.ts
+++ src/processes/trails.ts
@@ -6,10 +6,16 @@ export class ContourTrail extends Process {
     phase = 0;
     hoverShapes: Shape[] = [];
 
+    // Temp vectors for calc results.
+    pa = Vec2.newZero();
+    pb = Vec2.newZero();
+
     init() {
         const numShapes = this.numShapes || 10;
         for (let i = 0; i < numShapes; i++) {
-            this.hoverShapes.push(new Shape([this.shape.points[0].clone()], {
+            const p = this.shape.points[0];
+            const p1 = p.clone(), p2 = p.clone();
+            this.hoverShapes.push(new Shape([p1, Vec2.add(p2, p2, Vec2.one)], {
                 translation: this.shape.translation,
                 strokeStyle: this.shape.strokeStyle,
                 lineWidth: this.minLineWidth + (this.maxLineWidth - this.minLineWidth)*i/numShapes
@@ -26,13 +32,19 @@ export class ContourTrail extends Process {
         const { points } = this.shape;
 
         for (let i = 0; i < hoverShapes.length - 1; i++) {
-            hoverShapes[i].points[0].x = hoverShapes[i + 1].points[0].x;
-            hoverShapes[i].points[0].y = hoverShapes[i + 1].points[0].y;
+            const dst = hoverShapes[i], src = hoverShapes[i + 1];
+            dst.points[0].x = src.points[0].x;
+            dst.points[0].y = src.points[0].y;
+            dst.points[1].x = src.points[1].x;
+            dst.points[1].y = src.points[1].y;
         }
+        const last = hoverShapes[hoverShapes.length - 1];
+        const from = points[this.phase], to = points[(this.phase + 1)%points.length];
+        Vec2.lerp(last.points[0], from, to, progress);
         Vec2.lerp(
-            hoverShapes[hoverShapes.length - 1].points[0],
-            points[this.phase],
-            points[(this.phase + 1)%points.length],
+            last.points[1],
+            Vec2.add(this.pa, from, Vec2.one),
+            Vec2.add(this.pb, to, Vec2.one),
             progress);
         if (progress === 1) nextPhase(this, points.length);
 
